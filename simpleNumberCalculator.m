7b904d007e2896f4644bdaf27e3ce621a8a6959e
Sarvin Nami <srvn0nm@gmail.com>
2022-07-19 23:08:59 +0430

    simple code for calculating the range of grade

---
+++ simpleNumberCalculator.m
@@ -0,0 +1,16 @@
+clear
+clc
+g=input('enter your grade here: ');
+if and(g>=90,g<=100)
+  disp('Your grade is A. Congradulations!')
+elseif and(g<90,g>=80)
+  disp('Your grade is B. Welldone!')
+elseif and(g<80,g>=70)
+  disp('Your grade is C. You can achieve better!')
+elseif and(g<70,g>=60)
+  disp('Your grade is D. Work harder!')
+elseif g<40
+  disp('Your grade is F. Sorry, you failed!')
+else
+  disp('You just passed the exam!')
+end

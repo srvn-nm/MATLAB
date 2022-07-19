c6461d73f86fd93a2d4f97b0a197d4a03e1f282e
Sarvin Nami <srvn0nm@gmail.com>
2022-07-19 22:29:19 +0430

    multiple ifs and using and operator

--- ifAndElse.m
+++ ifAndElse.m
@@ -2,9 +2,20 @@ clear
 clc
 n=input('enter n here: ');
 cost=n*10;
-if n>=5
+if and(n>=5,n<10)
   cost=0.95*cost
+  discount=0.05
+elseif and(n>=10,n<15)
+  cost=0.9*cost
+  discount=0.1
+elseif and(n>=15,n<20)
+  cost=0.85*cost
+  discount=0.15
+elseif n>=20
+  cost=0.8*cost
+  discount=0.2
 else
   cost=cost
+  discount=0
 end
 

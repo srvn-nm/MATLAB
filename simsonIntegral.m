64ace317e578ef8c0581b3ac0ce7f6f054984954
Sarvin Nami <srvn0nm@gmail.com>
2022-08-04 02:03:58 +0430

    debugging

--- simsonIntegral.m
+++ simsonIntegral.m
@@ -5,19 +5,13 @@ a = input('Enter a here: ');
 b = input('Enter b here: ');
 n = input('Enter n here: ');
 
-f = @(x,e) (e*x+sin(x))
+F = @(x,e) (e*x+sin(x))
 
 h = (b - a)/n;
 
 x=linspace(a,b,n);
 
+I = 0;
 for i = 1:n-1
-I(i)= (h/3)*(f(x(i))+(4*f((x(i)+x(i+1))/2))+f(x(i+1)));
+    I = I + h/3*(F(x(i)) + 4*F((x(i)+x(i+1))/2) + F(x(i+1)));
 end
-answer1 = sum(I)
-disp(I)
-
-val2=ff(length(xx));
-sum=val1+val2+sum;
-
-result=sum*h/3;

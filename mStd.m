b552f469e28f264f20b1f46550ed7e109110161d
Sarvin Nami <srvn0nm@gmail.com>
2022-09-01 00:21:15 +0430

    Signed-off-by: Sarvin Nami <srvn0nm@gmail.com> editting my function

--- mStd.m
+++ mStd.m
@@ -1,14 +1,14 @@
-clear
-clc
-A=input('Enter the matrix here: ');
+
+function myStd=mStd(A)
 [r,c]=size(A);
 x=0;
 for i=1:(r*c)
   x=x+A(i);
-end
+endfor
 x=x/(r*c);
 myStd=0;
 for j=1:(r*c)
   myStd=myStd+((A(j)-x)^2);
-end
+endfor
 myStd=sqrt(myStd/((r*c)-1))
+end
